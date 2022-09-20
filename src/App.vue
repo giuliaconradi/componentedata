3eb15664eab66d29a5ea4a3ed5400a678b19605a
GIULIA LARISSA CONRADI <giulia.conradi@alunos.sc.senac.br>
2022-09-19 21:56:51 -0300

    componente data

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <input type="date" v-model="data" />
+    <componente-data :dataParam="data"></componente-data>
+  </div>
+</template>
+
+<script>
+import ComponenteData from "./components/ComponenteData.vue";
+
+export default {
+  data() {
+    return {
+      data: null,
+    };
+  },
+  components:{ ComponenteData },
+};
+</script>
+
+<style>
+#ComponenteDataapp {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ComponenteData.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>Resultado{{getExtenso()}}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    dataParam: String,
+  },
+  data() {
+    return {
+      mes: [
+        "jan",
+        "fev",
+        "mar",
+        "abr",
+        "mai",
+        "jun",
+        "jul",
+        "ago",
+        "set",
+        "out",
+        "nov",
+        "dez",
+      ],
+    };
+  },
+  methods: {
+    getExtenso() {
+      if (this.dataParam) {
+        const d = this.dataParam.split('-');
+        return d[2] + " " + this.mes[parseInt(d[1] - 1)] + " " + d[0];
+      }
+      return "";
+    },
+  },
+};
+</script>
+
+<style>
+</style>
